06203fd3d60d8ee7e60a9c50ca37a7619ad2e079
ipektahan <tahanipek@gmail.com>
2024-07-01 22:31:18 +0300

    changes

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))         
