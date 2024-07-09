f949467b7bc2938b19bcd478807260258ea5274b
ipektahan <tahanipek@gmail.com>
2024-07-09 21:10:40 +0300

    Added Error Messages

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -11,7 +11,7 @@ namespace BulkyWeb.Models
         [DisplayName("Category Name ")]
         public string Name { get; set; }
         [DisplayName("Display Order")]
-        [Range(1, 100)]
+        [Range(1, 100, ErrorMessage="Display order must be between 1-100")]
         public int DisplayOrder { get; set; }
     }
 }
